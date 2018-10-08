fb6dc5c31b2eb2975253e0da0d9f21a34f90369a
Ubuntu <uvic@ubuntu.(none)>
2018-10-08 07:31:35 -0700

    exercici1_arreglatfinal

--- seqClassexercise1.sh
+++ seqClassexercise1.sh
@@ -1,10 +1,14 @@
-#Final version, it detects both, lower and upper cases.
+#Final version, it detects both, lower and upper cases and if there's T and U.
 #!/bin/bash
 seq=$1
 seq=$(echo $seq | tr a-z A-Z)  # upper-case convertion
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
   if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
+    if [[ $seq =~ U ]]; then  # detects if there's a sequence with both u nd t
+      echo "The sequence is not DNA nor RNA"
+    else
+      echo "The sequence is DNA"
+    fi
   elif [[ $seq =~ U ]]; then
     echo "The sequence is RNA"
   else
